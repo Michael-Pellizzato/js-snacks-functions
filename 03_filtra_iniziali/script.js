5ab1e2f8fb7d257ae034e4cfa4112c6354a36361
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-17 17:13:44 +0100

    esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,17 +1,22 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-const nomi = []
+const arraynomi = []
 
 // Dichiara la funzione qui.
-function oneletter(array, lettera){
+function estrainomi(array, lettera){
 
-    for(let i= 0; i < array.length; i++)
-        if (array[i] [0].touppercase)
+    for(let i= 0; i < array.length; i++){ 
+        if (array[i] [0].touppercase() === lettera.touppercase()){
+            arraynomi.push( array[i])
+        }
+    }
+       
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+estrainomi( names, "L")
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log( arraynomi)
