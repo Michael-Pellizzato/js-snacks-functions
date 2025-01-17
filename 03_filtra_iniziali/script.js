675fd58d63ca1b22438777f477f616b204b5fcc6
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-17 16:52:46 +0100

    1

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,10 +1,14 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const nomi = []
 
 // Dichiara la funzione qui.
+function oneletter(array, lettera){
 
+    for(let i= 0; i < array.length; i++)
+        if (array[i] [0].touppercase)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
