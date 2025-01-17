4d05fa6deb9feba36ed589fbb7e3b7a0c78bbd7d
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-17 17:26:39 +0100

    rstart post ragionamento

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,20 +1,14 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const lettere = []
+
 
 
 // Dichiara la funzione qui.
-function letturaArray(arraynomi){
-    for( let i = 0; i < arraynomi.length; i++){
-        console.log( names[i].charAt(0) )
-        lettere.push( names[i].charAt(0))
-    }
-}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-letturaArray( names)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,22 +1,13 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-const arraynomi = []
+
 
 // Dichiara la funzione qui.
-function estrainomi(array, lettera){
 
-    for(let i= 0; i < array.length; i++){ 
-        if (array[i] [0].touppercase() === lettera.touppercase()){
-            arraynomi.push( array[i])
-        }
-    }
-       
-}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-estrainomi( names, "L")
+
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
-console.log( arraynomi)
